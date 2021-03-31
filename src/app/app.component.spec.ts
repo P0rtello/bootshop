67871d8a036c1bc2cc7024e02a47bba230a3e96b
P0rtello <47140374+P0rtello@users.noreply.github.com>
2021-03-31 15:04:14 +0200

    2
    2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'fitnex'`, () => {
+  it(`should have as title 'Boot shop'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('fitnex');
+    expect(app.title).toEqual('Boot shop');
   });
 
   it('should render title', () => {
